f9bb3680fa1b23e85dafac0db0928dffe55f64a5
Alicia <aliciajimenez@Alicias-MBP.attlocal.net>
2022-06-08 01:30:35 -0400

    html cleanup, refactor script.js, add .gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,143 +1,128 @@
-var apiKey = "d1e2d0763204896fd894698f5c6e27ee";
-var today = moment().format('L');
-var searchHistoryList = [];
+const apiKey = '7b88214d1981eecc55e37cf3d2d98552';
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
+let today = moment().format('L');
+let searchHistoryList = [];
 
-// function for current condition
-function currentCondition(city) {
+let getCurrentCondition = (city) => {
+    let weatherQueryUrl = `${baseUrl}/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    $.get(weatherQueryUrl, (response) => {
+        $('#weatherContent').css('display', 'block');
+        $('#cityDetail').empty();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(cityWeatherResponse) {
-        console.log(cityWeatherResponse);
-        
-        $("#weatherContent").css("display", "block");
-        $("#cityDetail").empty();
-        
-        var iconCode = cityWeatherResponse.weather[0].icon;
-        var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+        let iconCode = response.weather[0].icon;
+        let iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-        var currentCity = $(`
-            <h2 id="currentCity">
-                ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
-            </h2>
-            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
-            <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
-            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
-        `);
+        let currentCity = `
+            <h2 id="currentCity">${response.name} ${today} <img src="${iconUrl}" alt="${response.weather[0].description}"></h2>
+            <p>Temperature: ${response.main.temp} °F</p>
+            <p>Humidity: ${response.main.humidity}\%</p>
+            <p>Wind Speed: ${response.wind.speed} MPH</p>
+        `;
 
-        $("#cityDetail").append(currentCity);
+        $('#cityDetail').append(currentCity);
 
-        var lat = cityWeatherResponse.coord.lat;
-        var lon = cityWeatherResponse.coord.lon;
-        var uviQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-        $.ajax({
-            url: uviQueryURL,
-            method: "GET"
-        }).then(function(uviResponse) {
-            console.log(uviResponse);
-
-            var uvIndex = uviResponse.value;
-            var uvIndexP = $(`
-                <p>UV Index: 
-                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
-                </p>
-            `);
-
-            $("#cityDetail").append(uvIndexP);
-
-            futureCondition(lat, lon);
-
-// UV index weather condition by color
-            if (uvIndex >= 0 && uvIndex <= 2) {
-                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
-            } else if (uvIndex >= 3 && uvIndex <= 5) {
-                $("#uvIndexColor").css("background-color", "#FFF300");
-            } else if (uvIndex >= 6 && uvIndex <= 7) {
-                $("#uvIndexColor").css("background-color", "#F18B00");
-            } else if (uvIndex >= 8 && uvIndex <= 10) {
-                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
-            } else {
-                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
-            };  
-        });
+        getUvIndex(response.coord.lat, response.coord.lon);
     });
 }
 
-// function for future condition
-function futureCondition(lat, lon) {
+let getUvIndex = (lat, lon) => {
+    let uviQueryUrl = `${baseUrl}/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    var futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+    $.get(uviQueryUrl, (response) => {
+        let uvIndex = response.value;
+    
+        let uvIndexBlock = `
+            <p>UV Index: 
+                <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+            </p>
+        `;
 
-    $.ajax({
-        url: futureURL,
-        method: "GET"
-    }).then(function(futureResponse) {
-        console.log(futureResponse);
-        $("#fiveDay").empty();
+        $('#cityDetail').append(uvIndexBlock);
+
+        switch (true) {
+            case uvIndex >= 0 && uvIndex <= 2:
+                $('#uvIndexColor').css('background-color', '#3EA72D').css('color', 'white');
+                break;
+            case uvIndex >= 3 && uvIndex <= 5:
+                $('#uvIndexColor').css('background-color', '#FFF300');
+                break;
+            case uvIndex >= 6 && uvIndex <= 7:
+                $('#uvIndexColor').css('background-color', '#F18B00');
+                break;
+            case uvIndex >= 8 && uvIndex <= 10:
+                $('#uvIndexColor').css('background-color', '#E53210').css('color', 'white');
+                break;
+            default:
+                $('#uvIndexColor').css('background-color', '#B567A4').css('color', 'white'); 
+        }
+
+        getFutureCondition(lat, lon);
+    });
+}
+
+let getFutureCondition = (lat, lon) => {
+    let futureQueryUrl = `${baseUrl}/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+
+    $.get(futureQueryUrl, (response) => {
+        $('#fiveDay').empty();
         
         for (let i = 1; i < 6; i++) {
-            var cityInfo = {
-                date: futureResponse.daily[i].dt,
-                icon: futureResponse.daily[i].weather[0].icon,
-                temp: futureResponse.daily[i].temp.day,
-                humidity: futureResponse.daily[i].humidity
+            let cityInfo = {
+                date: response.daily[i].dt,
+                icon: response.daily[i].weather[0].icon,
+                temp: response.daily[i].temp.day,
+                humidity: response.daily[i].humidity,
             };
 
-            var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-            var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
+            let currentDate = moment.unix(cityInfo.date).format('MM/DD/YYYY');
+            let iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${response.daily[i].weather[0].main}">`;
 
-            var futureCard = $(`
+            let futureCardBlock = `
                 <div class="pl-3">
                     <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
                         <div class="card-body">
-                            <h5>${currDate}</h5>
+                            <h5>${currentDate}</h5>
                             <p>${iconURL}</p>
                             <p>Temp: ${cityInfo.temp} °F</p>
                             <p>Humidity: ${cityInfo.humidity}\%</p>
                         </div>
                     </div>
                 <div>
-            `);
+            `;
 
-            $("#fiveDay").append(futureCard);
+            $('#fiveDay').append(futureCardBlock);
         }
-    }); 
+    });
 }
 
-// add on click event listener 
-$("#searchBtn").on("click", function(event) {
-    event.preventDefault();
+// Add event listeners
+$(document).ready(() => {
+    let searchHistory = JSON.parse(localStorage.getItem('city'));
 
-    var city = $("#enterCity").val().trim();
-    currentCondition(city);
+    if (searchHistory !== null) {
+        let lastSearchedIndex = searchHistory.length - 1;
+        let lastSearchedCity = searchHistory[lastSearchedIndex];
+        getCurrentCondition(lastSearchedCity);
+    }
+});
+
+$(document).on('click', '.list-group-item', (event) => {
+    let city = event.target.textContent;
+    getCurrentCondition(city);
+});
+
+$('#searchBtn').on('click', (event) => {
+    event.preventDefault();
+    let city = $('#enterCity').val().trim();
+
+    getCurrentCondition(city);
+
     if (!searchHistoryList.includes(city)) {
         searchHistoryList.push(city);
-        var searchedCity = $(`
-            <li class="list-group-item">${city}</li>
-            `);
-        $("#searchHistory").append(searchedCity);
+        let searchedCity = `<li class="list-group-item">${city}</li/>`;
+        $('#searchHistory').append(searchedCity);
     };
     
-    localStorage.setItem("city", JSON.stringify(searchHistoryList));
-    console.log(searchHistoryList);
-});
-
-$(document).on("click", ".list-group-item", function() {
-    var listCity = $(this).text();
-    currentCondition(listCity);
-});
-
-$(document).ready(function() {
-    var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
-
-    if (searchHistoryArr !== null) {
-        var lastSearchedIndex = searchHistoryArr.length - 1;
-        var lastSearchedCity = searchHistoryArr[lastSearchedIndex];
-        currentCondition(lastSearchedCity);
-        console.log(`Last searched city: ${lastSearchedCity}`);
-    }
+    localStorage.setItem('city', JSON.stringify(searchHistoryList));
 });
